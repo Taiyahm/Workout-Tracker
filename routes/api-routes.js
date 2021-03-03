1b3866115e6870437726baae684a0e8468cf2e93
Taiyahm <taiyahm23@gmail.com>
2021-03-03 15:03:34 -0500

    updated api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,46 +2,45 @@ const db = require('../models');
 const router = require('express').Router();
 
 
-router.get("/api/workouts", (req, res) => {
+router.get('/api/workouts', (req, res) => {
 	db.Workout.find({})
 		.then((dbWorkout) => {
 			res.json(dbWorkout);
 		})
-		.catch((err) => {
-			res.json(err);
+		.catch(({ message }) => {
+			console.log(message);
 		});
 });
 
-router.post("/api/workouts", ({ body }, res) => {
+router.post('/api/workouts', ({ body }, res) => {
 	db.Workout.create(body)
 		.then((dbWorkout) => {
 			res.json(dbWorkout);
 		})
-		.catch((err) => {
-			res.json(err);
+		.catch(({ message }) => {
+			console.log(message);
 		});
 });
 
 
-
-router.put("/api/workouts/:id", ({ body, params }, res) => {
-	db.Workout.findOneAndUpdate( params.id , { $push: { exercises: body } })
+router.put('/api/workouts/:id', (req, res) => {
+	db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } }, { new: true })
 		.then((dbWorkout) => {
 			res.json(dbWorkout);
 		})
-		.catch((err) => {
-			res.json(err);
+		.catch(({ message }) => {
+			console.log(message);
 		});
 });
 
 
-router.get("/api/workouts/range", (req, res) => {
+router.get('/api/workouts/range', (req, res) => {
 	db.Workout.find({})
 		.then((dbWorkout) => {
 			res.json(dbWorkout);
 		})
-		.catch((err) => {
-			res.json(err);
+		.catch(({ message }) => {
+			console.log(message);
 		});
 });
 
